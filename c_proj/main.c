0e4ba82bb2dbedae15b2fc0ce4b6aa9d33d8b8bd
saashimi <kevin@saavedra.io>
2019-10-11 13:23:32 -0700

    Working fizzbuzz

--- c_proj/main.c
+++ c_proj/main.c
@@ -1,18 +1,33 @@
 #include <stdio.h>
 
-
-char * func( void );
+char* fizzbuzz( int x );
 
 int main() {
     char *res;
-    res = func();
-    printf("%s" , res);
+    int i;
+
+    for ( i = 1; i < 101; i ++ ) {
+        res = fizzbuzz( i );
+        printf("%s\n" , res);
+    }
 
     return 0;
 }
 
-char * func( void )
+char* fizzbuzz( int x )
 {
-    static char str[] = "blah";
-    return str;
+    static char bangv[] = "Bang!";
+    static char fizzv[] = "Fizz!";
+    static char buzzv[] = "Buzz!";
+    static char c[10];
+
+    if ( x % 15 == 0 )
+        return bangv;
+    else if ( x % 3 == 0 )
+        return fizzv;
+    else if ( x % 5 == 0 )
+        return buzzv;
+    else
+        sprintf( c, "%d", x );
+        return c;
 }
