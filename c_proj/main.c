071edd6f78d51e4d3bbc67fd42f3d5789747ed5f
saashimi <kevin@saavedra.io>
2019-10-11 11:22:09 -0700

    Pointer noodling

--- c_proj/main.c
+++ c_proj/main.c
@@ -1,37 +1,18 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-/* function declaration */
-char fizzbuzz(int x);
+
+char * func( void );
 
 int main() {
-    /* local variable definition */
-    int x = 15;
-    char ret;
-
-    /* calling FizzBuzz */
-    ret = fizzbuzz(x);
-    printf("%s", ret);
+    char *res;
+    res = func();
+    printf("%s" , res);
 
     return 0;
 }
 
-/* function returning the max between two numbers */
-char fizzbuzz(int x) {
-
-    /* local variable declaration */
-    char buffer[50];
-    char bang[] = "Bang!";
-    char fizz[] = "Fizz!";
-    char buzz[] = "Buzz!";
-
-    if ( x % 15 == 0)
-        return *bang;
-    else if ( x % 3 == 0)
-        return *fizz;
-    else if ( x % 5 == 0)
-        return *buzz;
-    else
-        sprintf(buffer, "%d", x);
-        return *buffer;
+char * func( void )
+{
+    static char str[] = "blah";
+    return str;
 }
