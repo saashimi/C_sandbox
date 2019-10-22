b71b6731a138b3de99f40b255e3de4adb2dcb70e
saashimi <kevin@saavedra.io>
2019-10-22 21:42:24 +0000

    example malloc

--- memory_alloc/mem.c
+++ memory_alloc/mem.c
@@ -1,19 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-/*
-int* func() {
-	int* mem = malloc(1024);
-	return mem;
-}*/
+#include <string.h>
 
 int main() {
-	int i, n;
+	char a[6] = "herpa";
+	char b[6] = "derpa";
+	char *c;
 
-	for (i = 1; i <= 10; i++) {
-		n = rand() % 100 + 1;
-		printf("%d\n", n);
-	}
+	c = malloc(11);	
+	strcpy( c, a );
+	strcat( c, b );	
+	
+	printf("%s\n", c);
+	printf("\nSize of a: %ld", sizeof(a));
+	printf("\nSize of b: %ld", sizeof(a));
+	printf("\nSize of c: %ld", sizeof(a));
+	free(c);
+	printf("\nSize of c when freed: %ld\n", sizeof(c));
+	printf("\nherpaderpa %ld\n", sizeof("herpaderpa"));
 	return 0;
 	
 }
+
+
