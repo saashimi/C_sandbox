afa754bfe877f7ff494fbdf6076954f8a1292f81
saashimi <kevin@saavedra.io>
2019-10-23 15:43:41 -0700

    mem alloc testing

--- memory_alloc/mem.c
+++ memory_alloc/mem.c
@@ -3,21 +3,44 @@
 #include <string.h>
 
 int main() {
-	char a[6] = "herpa";
-	char b[6] = "derpa";
-	char *c;
+	char *raw1; 
+	char *raw2;
+	char *concat;
+	/*char str1;
+	char str2;*/
 
-	c = malloc(11);	
-	strcpy( c, a );
-	strcat( c, b );	
+	raw1 = malloc(64);
+	raw2 = malloc(64);
+
+	printf( "Enter a string: >> ");
+	scanf("%s", raw1);
+
+	printf("Enter a second string: >> ");
+	scanf("%s", raw2);
+
+	printf("\nSize of raw1: %ld", sizeof(raw1));
+	printf("\nSize of raw2: %ld", sizeof(raw2));
+
+	int raw1sz = sizeof(raw1);
+	raw1 = realloc(raw1, raw1sz );
+	printf("\nSize of realloc raw1: %ld", sizeof(raw1));
+
+	int raw2sz = sizeof(raw2);
+	raw2 = realloc(raw2, raw2sz);	
+	printf("\nSize of realloc raw2: %ld", sizeof(raw2));
 	
-	printf("%s\n", c);
-	printf("\nSize of a: %ld", sizeof(a));
-	printf("\nSize of b: %ld", sizeof(a));
-	printf("\nSize of c: %ld", sizeof(a));
-	free(c);
-	printf("\nSize of c when freed: %ld\n", sizeof(c));
-	printf("\nherpaderpa %ld\n", sizeof("herpaderpa"));
+	concat = malloc(sizeof(raw1sz + raw2sz));
+	printf("\nSize of concat: %ld", sizeof(concat));
+
+	strcpy( concat, raw1 );
+	strcat( concat, raw2 );	
+	printf("\n%s\n", concat);
+	
+	free(raw1);
+	free(raw2);
+	free(concat);
+	printf("\nSize of concat when freed: %ld\n", sizeof(concat));
+
 	return 0;
 	
 }
