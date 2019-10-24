855b057c22776316db5718f9b6b8b6b70e444b08
saashimi <kevin@saavedra.io>
2019-10-23 18:34:32 -0700

    Fix length calculations

--- memory_alloc/mem.c
+++ memory_alloc/mem.c
@@ -16,19 +16,19 @@ int main() {
 	printf("Enter a second string: >> ");
 	scanf("%s", raw2);
 
-	printf("\nSize of raw1: %ld", sizeof(raw1) * (strlen(raw1) + 1));
-	printf("\nSize of raw2: %ld", sizeof(raw2) * (strlen(raw1) + 1));
+	printf("\nSize of raw1: %ld", (strlen(raw1) + 1));
+	printf("\nSize of raw2: %ld", (strlen(raw1) + 1));
 
 	int raw1sz = strlen(raw1);
 	raw1 = realloc(raw1, raw1sz + 1);
-	printf("\nSize of realloc raw1: %ld", sizeof(raw1) * strlen(raw1) + 1);
+	printf("\nSize of realloc raw1: %ld", strlen(raw1) + 1);
 
 	int raw2sz = strlen(raw2);
 	raw2 = realloc(raw2, raw2sz + 1);	
-	printf("\nSize of realloc raw2: %ld", sizeof(raw2));
+	printf("\nSize of realloc raw2: %ld", strlen(raw2) + 1);
 	
 	concat = malloc(raw1sz + raw2sz + 1);
-	printf("\nSize of concat: %ld", sizeof(concat) * strlen(concat) + 1);
+	printf("\nSize of concat: %ld", strlen(concat) + 1);
 
 	strcpy( concat, raw1 );
 	strcat( concat, raw2 );	
@@ -37,7 +37,7 @@ int main() {
 	free(raw1);
 	free(raw2);
 	free(concat);
-	printf("\nSize of concat when freed: %ld\n", sizeof(concat) * strlen(concat) + 1);
+	printf("\nSize of concat when freed: %ld\n", strlen(concat) + 1);
 	
 	return 0;
 	
