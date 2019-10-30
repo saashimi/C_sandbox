324d9051babda05000a886faa54c408a3d51b8b6
saashimi <kevin@saavedra.io>
2019-10-30 07:56:19 -0700

    Add inline linked list notes

--- linked_list/linked_list.c
+++ linked_list/linked_list.c
@@ -3,25 +3,26 @@
 struct LinkedList{
     int data;
     struct LinkedList *next;
-    /* *next is a self-referencing pointer to struct LinkedList */
+    //*next is a self-referencing pointer to struct LinkedList
 };
 
-typedef struct LinkedList *node; /* Define node as pointer of data type struct
-LinkedList */
+typedef struct LinkedList *node; 
+//Define node as pointer of data type struct LinkedList
 
 node createNode(){
-    node temp; // declare a node
+    node temp; // declare a node temp
     temp = (node) malloc ( sizeof( struct LinkedList ) );
-    /* allocate memory using malloc */
-    temp->next = NULL; //make next point to NULL
+    //allocate memory using malloc
+    temp->next = NULL; 
+    // (*temp).next points to NULL
     return temp; // return the new node
 }
 
 node addNode( node head, int value ){
-    node temp, p; // declare two nodes temp and p
+    node temp, p; // declare two nodes: temp and p
     temp = createNode(); 
-    /* create node will return a new node with data = value and next pointing
-    to null */
+    // create node will return a new node with data = value and next pointing
+    // to null 
     temp->data = value; // add element's value to data part of node
     if (head == NULL){
         head = temp; // when linked list is empty
