3bc03744ee5e98a5f78dfc18e828c3d987bbdf3d
saashimi <kevin@saavedra.io>
2020-12-24 15:20:04 -0800

    update comments

--- c_sandbox/main.c
+++ c_sandbox/main.c
@@ -5,9 +5,9 @@
 int main(void) {
     char array[10][6] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
     
-    char **strings = malloc(10 * 10); //pointer to pointer
+    char **strings = malloc(10 * 10); //pointer to pointer; max size specified
     for (int i = 0; i < 10; ++i) {
-        strings[i] = malloc(6);
+        strings[i] = malloc(6); // length of indiv. string
         strcpy(strings[i], array[i]);
     }
     
