67f20b5a45f76ccb5124b88eb37c75207d0413fe
saashimi <kevin@saavedra.io>
2020-12-24 12:32:25 -0800

    fixed mem test

--- c_sandbox/main.c
+++ c_sandbox/main.c
@@ -3,27 +3,14 @@
 #include <stdio.h>
 
 int main(void) {
-    //char *malloc_ptr;
     char array[10][10] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
-    //char *cpy_array;
-    //char cpy_array[10][10];
-    //char *cpy_array_ptr = &cpy_array[10][10];
+    char cpy_array[10][10];
     
-    char *cpy_array = malloc(10 * 10 * sizeof(char));
     for (int i = 0; i < 10; ++i) {
-        //cpy_array[i] = array[i];
-        
-        strcpy(&cpy_array[i], array[i]);
-        //free(cpy_array[i]);
-        
-        //malloc_ptr = malloc(10);
-        //strcpy(malloc_ptr, array[i]);
-        //strcpy(cpy_array[i], malloc_ptr);
+        strcpy(cpy_array[i], array[i]);
     }
     for (int i= 0; i < 10; ++i)
-        printf("%c\n", cpy_array[i]);
+        printf("%s\n", cpy_array[i]);
     
-    free(cpy_array);
-
     return 0;
 }
